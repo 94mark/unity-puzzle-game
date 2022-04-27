51b49a7fdad777bac8f233c0cb421750eab4f6ec
94mark <90877724+94mark@users.noreply.github.com>
2022-04-27 12:06:40 +0900

    Update

--- puzzle2048/Assets/Scripts/GameManager.cs
+++ puzzle2048/Assets/Scripts/GameManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,7 +5,9 @@ public class GameManager : MonoBehaviour
 {
     public GameObject[] n;
 
+    bool wait;
     int x, y;
+    Vector3 firstPos, gap;
     GameObject[,] Square = new GameObject[4, 4];
     // Start is called before the first frame update
     void Start()
@@ -20,6 +20,44 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            wait = true;
+            firstPos = Input.GetMouseButtonDown(0) ? Input.mousePosition : (Vector3)Input.GetTouch(0).position;
+        }
+
+        if(Input.GetMouseButton(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved))
+        {
+            gap = (Input.GetMouseButton(0) ? Input.mousePosition : (Vector3)Input.GetTouch(0).position) - firstPos;
+            if (gap.magnitude < 100) return;
+            gap.Normalize();
+
+            if(wait)
+            {
+                wait = false;
+                // 위
+                if (gap.y > 0 && gap.x > -0.5f && gap.x < 0.5)
+                {
+
+                }
+                // 아래
+                else if (gap.y < 0 && gap.x > -0.5f && gap.x < 0.5f)
+                {
+
+                }
+                // 오른쪽
+                else if (gap.x < 0 && gap.y > -0.5f && gap.y < 0.5f)
+                {
+
+                }
+                // 왼쪽
+                else if (gap.x < 0 && gap.y > -0.5f && gap.y < 0.5f)
+                {
+
+                }
+                else return;
+            }
+        }
     }
 
     void Spawn()
@@ -33,6 +71,7 @@ public class GameManager : MonoBehaviour
         }
         //(0,0) 좌표는 (-1.8, -1.8), 한칸씩 이동 시 1.2 만큼 이동
         Square[x, y] = Instantiate(Random.Range(0, 8) > 0 ? n[0] : n[1], new Vector3(1.2f * x - 1.8f, 1.2f * y - 1.8f, 0), Quaternion.identity);
+        Square[x, y].GetComponent<Animator>().SetTrigger("Spawn");
     }
 
     public void Restart()
